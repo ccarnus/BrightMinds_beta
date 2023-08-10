0b3ee475b3b322dad30b460bbe11f374bf6522fd
Clément CARNUS <clement.carnus@exotec.com>
2023-08-09 21:17:42 -0400

    work on the random catchup version

--- components/Cast/CatchUpScreen.js
+++ components/Cast/CatchUpScreen.js
@@ -1,43 +1,84 @@
-import React, { useRef, useState } from 'react';
-import { View, StyleSheet, Button } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { View, ScrollView, StyleSheet, TouchableOpacity } from 'react-native';
 import { Video } from 'expo-av';
+import { Dimensions } from 'react-native';
 
 const CatchUpScreen = () => {
-  const video = useRef(null);
-  const [status, setStatus] = useState({});
+  const [videos, setVideos] = useState([]);
+  const [focusedIndex, setFocusedIndex] = useState(0);
+  const [isPlaying, setIsPlaying] = useState(true);
+
+  useEffect(() => {
+    // Fetch data from the endpoint
+    fetch('http://3.17.219.54/cast')
+      .then(response => response.json())
+      .then(data => setVideos(data))
+      .catch(error => console.error(error));
+  }, []);
+
+  const handleVideoPress = index => {
+    setFocusedIndex(index);
+    setIsPlaying(!isPlaying);
+  };
 
   return (
     <View style={styles.container}>
-      <Video
-        ref={video}
-        style={styles.video}
-        source={{
-          uri: 'http://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4',
+      <ScrollView
+        style={styles.scrollView}
+        horizontal
+        pagingEnabled
+        showsHorizontalScrollIndicator={false}
+        onMomentumScrollEnd={event => {
+          const offsetX = event.nativeEvent.contentOffset.x;
+          const index = Math.round(offsetX / windowWidth);
+          setFocusedIndex(index);
+          setIsPlaying(true);
         }}
-        useNativeControls
-        resizeMode="contain"
-        isLooping
-        onPlaybackStatusUpdate={status => setStatus(status)}
-      />
+      >
+        {videos.map((video, index) => (
+          <TouchableOpacity
+            key={index}
+            style={[
+              styles.videoContainer,
+              {
+                width: windowWidth,
+                height: windowHeight,
+                opacity: focusedIndex === index ? 1 : 0.7,
+              },
+            ]}
+            onPress={() => handleVideoPress(index)}
+          >
+            <Video
+              source={{ uri: video.casturl }}
+              shouldPlay={focusedIndex === index && isPlaying}
+              resizeMode="cover"
+              style={styles.video}
+            />
+          </TouchableOpacity>
+        ))}
+      </ScrollView>
     </View>
   );
 };
 
+const windowWidth = Dimensions.get('window').width;
+const windowHeight = Dimensions.get('window').height;
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    backgroundColor: '#ecf0f1',
+    backgroundColor: '#000',
   },
-  video: {
-    alignSelf: 'center',
-    width: 350,
-    height: 220,
+  scrollView: {
+    flex: 1,
   },
-  buttons: {
-    flexDirection: 'row',
+  videoContainer: {
     justifyContent: 'center',
     alignItems: 'center',
+  },
+  video: {
+    width: '100%',
+    height: '100%',
   },
 });
 
