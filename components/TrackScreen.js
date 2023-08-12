279d776165b70d683862f80fff24aab924288456
Clément CARNUS <clement.carnus@exotec.com>
2023-08-12 16:10:32 -0400

    cool changes

--- components/TrackScreen.js
+++ components/TrackScreen.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
-import { View, Text, StyleSheet, Dimensions } from 'react-native';
+import { View, Text, StyleSheet, Dimensions, Image, TouchableOpacity, ScrollView } from 'react-native';
 import { ProgressBar } from 'react-native-paper';
+import { Svg, Circle, Image as SvgImage } from 'react-native-svg';
 
 const TrackScreen = () => {
   const fields = [
@@ -38,21 +39,80 @@ const TrackScreen = () => {
   }, []);
 
   return (
-    <View style={styles.container}>
-      {fields.map((field, index) => (
-        <View key={index} style={styles.fieldContainer}>
-          <Text style={styles.fieldName}>{field.name}</Text>
-          <ProgressBar
-            progress={field.progress}
-            color={field.color}
-            style={styles.progressBar}
-            borderRadius={10}
-            borderWidth={0}
-            height={20}
-          />
+    <ScrollView contentContainerStyle={styles.container}>
+      {/* Cast Watching Time */}
+      <View style={styles.categoryContainer}>
+  <View style={styles.categoryTitleContainer}>
+    <Text style={styles.categoryTitle}>Cast Watching Time</Text>
+  </View>
+  <View style={styles.iconContainer}>
+    <Svg width={60} height={60}>
+      <Circle
+        cx={30}
+        cy={30}
+        r={27}
+        stroke="#ccc"
+        strokeWidth={6} // Adjust the thickness
+        fill="none"
+      />
+      <Circle
+        cx={30}
+        cy={30}
+        r={27}
+        stroke="#3498db"
+        strokeWidth={6} // Adjust the thickness
+        fill="none"
+        strokeDasharray="162" // Circumference of a circle with radius 27 (2 * pi * 27)
+        strokeDashoffset={162 * (1 - 0.7)} // Fill 70% of the circle
+        strokeLinecap="round"
+        transform="rotate(-90 30 30)"
+      />
+      <SvgImage
+        x={12} // Adjust the x coordinate for horizontal alignment
+        y={12} // Adjust the y coordinate for vertical alignment
+        width={36} // Adjust the width and height of the image
+        height={36}
+        href={require('../assets/Track_icons/clock.png')}
+      />
+    </Svg>
+  </View>
+</View>
+      {/* Demarcation */}
+      <View style={styles.demarcation} />
+
+      {/* Weekly Evaluation */}
+      <View style={styles.categoryContainer}>
+        <View style={styles.categoryTitleContainer}>
+          <Text style={styles.categoryTitle}>Weekly Evaluation</Text>
         </View>
-      ))}
-    </View>
+        <TouchableOpacity style={styles.button}>
+          <Text style={styles.buttonText}>Take the Test</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Demarcation */}
+      <View style={styles.demarcation} />
+
+      {/* Track Progress */}
+      <View style={styles.categoryContainer}>
+        <View style={styles.categoryTitleContainer}>
+          <Text style={styles.categoryTitle}>Track Progress</Text>
+        </View>
+        {fields.map((field, index) => (
+          <View key={index} style={styles.fieldContainer}>
+            <Text style={styles.fieldName}>{field.name}</Text>
+            <ProgressBar
+              progress={field.progress}
+              color={field.color}
+              style={styles.progressBar}
+              borderRadius={15}
+              borderWidth={0}
+              height={15}
+            />
+          </View>
+        ))}
+      </View>
+    </ScrollView>
   );
 };
 
@@ -60,16 +120,56 @@ const windowWidth = Dimensions.get('window').width;
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    flexGrow: 1,
     backgroundColor: '#f1f1f1',
+    padding: 20,
   },
-  heading: {
-    fontSize: 24,
+  categoryContainer: {
+    marginBottom: 20,
+    alignItems: 'center',
+  },
+  categoryTitleContainer: {
+    flexDirection: 'row',
+    alignItems: 'flex-start',
+    justifyContent: 'flex-start',
+    width: '100%',
+    marginBottom: 8,
+  },
+  categoryTitle: {
+    fontSize: 20,
     fontWeight: 'bold',
     color: '#1c1c1c',
-    marginBottom: 20,
+  },
+  iconContainer: {
+    width: 50,
+    height: 50,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginBottom: 8,
+  },
+  iconImage: {
+    width: 50,
+    height: 50,
+  },
+  valueText: {
+    fontSize: 18,
+    color: '#1c1c1c',
+  },
+  button: {
+    backgroundColor: '#3498db',
+    paddingVertical: 10,
+    paddingHorizontal: 20,
+    borderRadius: 10,
+  },
+  buttonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  demarcation: {
+    height: 1,
+    backgroundColor: '#ccc',
+    marginVertical: 20,
   },
   fieldContainer: {
     alignItems: 'center',
@@ -83,6 +183,8 @@ const styles = StyleSheet.create({
   },
   progressBar: {
     width: windowWidth - 100, // Adjust for spacing
+    height: 15,
+    borderRadius: 15,
   },
 });
 
