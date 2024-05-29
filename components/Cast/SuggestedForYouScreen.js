8f0052ddb88e73b7fc7ea2430ce14f282dd446c7
Clément CARNUS <clement.carnus@exotec.com>
2024-05-29 08:51:16 -0400

    changing display

--- components/Cast/SuggestedForYouScreen.js
+++ components/Cast/SuggestedForYouScreen.js
@@ -139,7 +139,7 @@ const SuggestedForYouScreen = ({ route, navigation }) => {
               ref={ref => (videoRefs.current[index] = ref)}
               source={{ uri: video.casturl }}
               shouldPlay={videoStatus[index]}
-              resizeMode="cover"
+              resizeMode="contain"
               style={styles.video}
               onPlaybackStatusUpdate={(status) => {
                 if (status.didJustFinish) {
